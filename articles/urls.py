9dfd6a7b559ca10fff5345e7c03a93c86f6d99a4
Shabari S Nair <shabarisnair123@gmail.com>
2020-09-11 18:40:34 +0530

    upto tut 16

--- articles/urls.py
+++ articles/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+app_name = 'articles'
 urlpatterns = [
     path('',views.article_list,name = 'list'),
     path('<slug:slug>/',views.article_detail, name = 'detail'),
