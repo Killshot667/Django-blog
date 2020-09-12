c5a758ec117dee77a793439e5c9955351ecd56b0
Shabari S Nair <shabarisnair123@gmail.com>
2020-09-12 20:40:38 +0530

    after tut 27

--- articles/views.py
+++ articles/views.py
@@ -18,9 +18,11 @@ def article_detail(request, slug):
 @login_required(login_url="accounts:login")
 def article_create(request):
     if request.method == 'POST':
-        form = forms.CreateArticle(request.POST,request.FILES)
+        form = forms.CreateArticle(request.POST, request.FILES)
         if form.is_valid():
-            #put in db
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
             return redirect("articles:list")
     else:
         form = forms.CreateArticle()
